ebad43c9c9704d101ae966e7bc149882435c14b2
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-02-22 21:05:20 +0530

    first commit

---
+++ src/test/java/io/hrushik09/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hrushik09.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
