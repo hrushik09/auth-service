7e887243c506333c5012919e6e4bd6d668a5e739
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-03-18 01:25:39 +0530

    add end-to-end test for creating clients

---
+++ src/test/java/io/hrushik09/authservice/clients/ClientEndToEndTest.java
@@ -0,0 +1,54 @@
+package io.hrushik09.authservice.clients;
+
+import io.hrushik09.authservice.setup.EndToEndTest;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@EndToEndTest
+public class ClientEndToEndTest {
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Nested
+    class Create {
+        @Test
+        void shouldCreateClientSuccessfully() {
+            given()
+                    .contentType(ContentType.JSON)
+                    .body("""
+                            {
+                            "id": "rc",
+                            "clientId": "client",
+                            "clientSecret": "secret",
+                            "scope": "OPENID",
+                            "redirectUri": "http://localhost:8080/authorized",
+                            "clientAuthenticationMethod": "CLIENT_SECRET_BASIC",
+                            "authorizationGrantType": "AUTHORIZATION_CODE"
+                            }
+                            """)
+                    .when()
+                    .post("/api/clients")
+                    .then()
+                    .statusCode(201)
+                    .body("id", equalTo("rc"))
+                    .body("clientId", equalTo("client"))
+                    .body("clientSecret", equalTo("secret"))
+                    .body("scope", equalTo("OPENID"))
+                    .body("redirectUri", equalTo("http://localhost:8080/authorized"))
+                    .body("clientAuthenticationMethod", equalTo("CLIENT_SECRET_BASIC"))
+                    .body("authorizationGrantType", equalTo("AUTHORIZATION_CODE"));
+        }
+    }
+}
