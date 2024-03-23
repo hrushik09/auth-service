0c5f2d7ff26b091f87ec0816fbb18f5ce7812f26
Hrushikesh Kandale <hrushikeshkandale09@gmail.com>
2024-03-23 18:13:46 +0530

    add missing valid annotation, add exception handler for MethodArgumentNotValidException

--- src/main/java/io/hrushik09/authservice/advice/ApplicationExceptionHandler.java
+++ src/main/java/io/hrushik09/authservice/advice/ApplicationExceptionHandler.java
@@ -6,14 +6,24 @@ import io.hrushik09.authservice.clients.exceptions.ClientIdAlreadyExistsExceptio
 import io.hrushik09.authservice.clients.exceptions.IdAlreadyExistsException;
 import io.hrushik09.authservice.users.exceptions.UsernameAlreadyExistsException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentsNotValid(MethodArgumentNotValidException e) {
+        return e.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+    }
+
     @ExceptionHandler({AuthorityAlreadyExists.class, UsernameAlreadyExistsException.class, IdAlreadyExistsException.class, ClientIdAlreadyExistsException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleAlreadyExists(RuntimeException e) {
--- src/main/java/io/hrushik09/authservice/clients/ClientController.java
+++ src/main/java/io/hrushik09/authservice/clients/ClientController.java
@@ -3,6 +3,7 @@ package io.hrushik09.authservice.clients;
 import io.hrushik09.authservice.clients.dto.CreateClientCommand;
 import io.hrushik09.authservice.clients.dto.CreateClientRequest;
 import io.hrushik09.authservice.clients.dto.CreateClientResponse;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +20,7 @@ public class ClientController {
     @PostMapping
     @PreAuthorize("hasAuthority('clients:create')")
     @ResponseStatus(HttpStatus.CREATED)
-    CreateClientResponse create(@RequestBody CreateClientRequest request) {
+    CreateClientResponse create(@RequestBody @Valid CreateClientRequest request) {
         CreateClientCommand cmd = new CreateClientCommand(request.id(), request.clientId(), request.clientSecret(), request.clientAuthenticationMethod(), request.scope(), request.redirectUri(), request.authorizationGrantType());
         return clientService.create(cmd);
     }
